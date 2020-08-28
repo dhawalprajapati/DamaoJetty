6e26fa26e3d89cf886fb0f16641466503e9a4375
DESKTOP-L30R06E\DhawalPrajapati <dhawalprajapati@ymail.com>
2020-08-28 16:43:03 +0100

    Contact page update

--- DamaoJettyWebProject/DamaoJetty.Web/Controllers/ContactController.cs
+++ DamaoJettyWebProject/DamaoJetty.Web/Controllers/ContactController.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services;
 
 namespace DamaoJetty.Web.Controllers
 {
@@ -20,5 +24,48 @@ namespace DamaoJetty.Web.Controllers
             Session["Login"] = null;
             return View();
         }
+
+        [HttpPost]
+        [WebMethod]
+        [ActionName("SendContactDetail")]
+        public string save(
+           string Message,
+           string Name,
+           string Email
+           )
+        {
+
+            string HostAdd = ConfigurationManager.AppSettings["Host"].ToString();
+            string Pass = ConfigurationManager.AppSettings["Password"].ToString();
+            string ToEmail = "info@damaojetty.co.uk";
+
+            MailMessage mailMessage = new MailMessage();
+
+            mailMessage.From = new MailAddress(Email);
+            mailMessage.Subject = "Customet feedback message from Contact form ";
+            mailMessage.Body = Message;
+            mailMessage.IsBodyHtml = true;
+            mailMessage.To.Add(new MailAddress(ToEmail));
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = HostAdd;
+
+            smtp.EnableSsl = true;
+            NetworkCredential NetworkCred = new NetworkCredential();
+            NetworkCred.UserName = "donotreply@damaojetty.co.uk";
+            NetworkCred.Password = Pass;
+            smtp.Credentials = NetworkCred;
+            smtp.Port = 587;
+            smtp.Send(mailMessage);
+
+            return "Success";
+        }
+
+
+        public void SendEmail(string customerEmail)
+        {
+            
+        }
+
     }
 }
